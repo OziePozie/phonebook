58f730140c283bfeda1b48c4bf53a9d69957e47d
timka <timkaa555@gmail.com>
2023-11-25 09:01:11 +0300

    contact refactor

--- Contact.py
+++ Contact.py
@@ -1,5 +1,6 @@
 class Contact:
-    def __init__(self, user, country_code, phone_number):
+    def __init__(self, user, name, country_code, phone_number):
         self.user = user
+        self.name = name
         self.country_code = country_code
         self.phone_number = phone_number
